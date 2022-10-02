811657a9ce287bbd7c244fc1c9af1981fb99789d
Dalal Alsabah <dasabah@gmail.com>
2022-10-02 20:40:03 +0300

    final

--- hr_pro.py
+++ hr_pro.py
@@ -1,22 +1,19 @@
-from curses import ACS_GEQUAL
-from unicodedata import name
-
 
 class Employee:
-   def __init__(self, name, age, salary, employment_years):
-    self.name= name
-    self.age= age
-    self.salary= salary 
-    self.employment_years= employment_years
+    def __init__(self, name, age, salary, employment_years):
+        self.name= name
+        self.age= age
+        self.salary= salary 
+        self.employment_years= employment_years
 
-    def __strg__(self):
+    def __str__(self):
         annual_salary= self.salary*12
-        print(f"Name: {self.name}, Age: {self.age}, Salary: {annual_salary}, Working Years: {self.employment_years},")
+        return (f"Name: {self.name}, Age: {self.age}, Salary: {annual_salary}, Working Years: {self.employment_years},")
 
 
-    def get_annual_salary(self, salary):
+    def get_annual_salary(self,):
         annualsalary=self.salary*12
-        return (annualsalary)
+        return annualsalary
 
     
 
@@ -27,7 +24,8 @@ def list_employee ():
     employee_three=Employee("moodi", 21, 120, 4)
     employee_four=Employee("haya", 27, 300, 6)
     employee_five=Employee("nouf", 30, 120, 2)
-    return (listemployee.append(employee_five,employee_four,employee_three,employee_two, employee_one))
+    listemployee.append(employee_five,employee_four,employee_three,employee_two, employee_one)
+    return listemployee
 
 
 
@@ -36,11 +34,11 @@ class Manager(Employee):
         super().__init__(name, age, salary, employment_years)    #Manager class here
         self.bonus_percentage= bonus_percentage
 
-    def get_bonus(self, salary, bonus_percentage):
-        return (salary* bonus_percentage)
+    def get_bonus(self):
+        return self.salary * self.bonus_percentage
     
     def __str__(self) -> str:
-        bonus= Manager.get_bonus
+        bonus=self.get_bonus()
         print (f"{super().__str__()} bonus {bonus}")
 
 def list_ofmanagers ():
@@ -48,6 +46,7 @@ def list_ofmanagers ():
     manager_one= Manager("ahmed", 40, 500, 10, 0.8)
     manager_two=Manager("laila", 20, 400, 3, 0.9)
     listofmangers.append(manager_one, manager_two)
+    return (listofmangers)
 
 
 
@@ -59,6 +58,8 @@ def list_ofmanagers ():
 def main():
     options=["Show Employees","Show Managers","Add An Employee","Add A Manager"
 	"Exit"]
+    listofmanagers= list_ofmanagers ()
+    listofemployee= list_employee () 
 
     print ("Welcome to HR Pro")
     print ("options")
@@ -72,43 +73,33 @@ def main():
     print (choose)
 
     print ("---------------------------------")
-
     if choose== 1:
-        print (list_employee())
-        return True
+            return (list_employee())
+            
     elif choose==2:
-        print (list_ofmanagers())
-        return True
+            return (list_ofmanagers())
+            
     elif choose==3:
-        name= input("whats the employees name?")
-        age= input("whats the employees age?")
-        salary= input(" whats the salary?")
-        employment_years= input("how many years of employment?")
-        employee_add= Employee(name, age, salary, employment_years)
-        list_employee.append(employee_add)
-        print (employee_add)
-        return True
+            name= input("whats the employees name?")
+            age= input("whats the employees age?")
+            salary= input(" whats the salary?")
+            employment_years= input("how many years of employment?")
+            employee_add= Employee(name, age, salary, employment_years)
+            listofemployee.append(employee_add)
+            return (employee_add)
+            
     elif choose==4:
-        name= input("whats the manager name?")
-        age= input("whats the manager age?")
-        salary= input(" whats the manager salary?")
-        employment_years= input("how many years of managment?")
-        bonus_percentage= input("bonus percentage?")
-        manager_add= Manager(name, age, salary, employment_years, bonus_percentage)
-        list_ofmanagers.append(manager_add)
-        print (manager_add)
-        return True
+            name= input("whats the manager name?")
+            age= input("whats the manager age?")
+            salary= input(" whats the manager salary?")
+            employment_years= input("how many years of managment?")
+            bonus_percentage= input("bonus percentage?")
+            manager_add= Manager(name, age, salary, employment_years, bonus_percentage)
+            listofmanagers.append(manager_add)
+            return (manager_add)
+        
     else:
-        return False
+        return ("programme off")  
 
 
-#bonus 
-def bonus(result):
-    if result is True:
-        return input
-    else:
-        print ("program off")
-
-bonus(main())
-
-
+print (main())
